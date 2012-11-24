62499c9fe9cf489317306100913fb75e141caba3
rundis <mrundberget@hotmail.com>
2012-11-24 13:45:22 +0100

    Pyntet litt på form oppsett i index.html

--- buster.js
+++ buster.js
@@ -1,9 +1,13 @@
 var config = module.exports;
 
 config["Browser tests"] = {
-  libs: ["web-app/js/external/**/*.js", "test/lib/underscore.js", "test/lib/cull.js"],
-  sources: ["web-app/js/**/*.js"],
-  tests: ["test/js/**/*-test.js"],
-  //testHelpers: ["test/lib/cull.js"],
-  environment: "browser"
+    libs: ["app/lib/angular/angular.js"],
+    sources: ["app/js/app.js",
+              "app/js/services.js",
+              "app/js/controllers.js",
+              "app/js/filters.js",
+              "app/js/directives"],
+    tests: ["test/unit/**/*-test.js"],
+    //testHelpers: ["test/lib/cull.js"],
+    environment: "browser"
 };
--- test/js/form-list-test.js
+++ test/js/form-list-test.js
@@ -1,40 +1,43 @@
 (function (S) {
-  "use strict";
+    "use strict";
 
-  buster.testCase("Form List", {
-    setUp: function () {
-      this.container = document.createElement("div");
+    buster.testCase("Form List", {
+        setUp: function () {
+            this.container = document.createElement("div");
 
-      this.buildForm = function () { return document.createElement("div"); };
+            this.buildForm = function () { return document.createElement("div"); };
 
-      var oldForm = document.createElement("div");
-      this.container.appendChild(oldForm);
+            var oldForm = document.createElement("div");
+            this.container.appendChild(oldForm);
 
-      sfk.setupFormList(this.container, this.buildForm);
-    },
 
-    "does nothing at first": function () {
-      assert.equals(this.container.childNodes.length, 1);
-    },
+            sfk.setupFormList(this.container, this.buildForm);
+        },
 
-    "adds new form when old form becomes dirty": function () {
-      $(this.container.firstChild).trigger("dirty");
+        "does nothing at first": function () {
+            assert.equals(this.container.childNodes.length, 1);
+        },
 
-      assert.equals(this.container.childNodes.length, 2);
-    },
+        "adds new form when old form becomes dirty": function () {
+            $(this.container.firstChild).trigger("dirty");
 
-    "does not add superflous forms": function () {
-      $(this.container.firstChild).trigger("dirty");
-      $(this.container.firstChild).trigger("dirty");
+            assert.equals(this.container.childNodes.length, 2);
+        },
 
-      assert.equals(this.container.childNodes.length, 2);
-    },
+        "does not add superflous forms": function () {
 
-    "adds new form when all are dirty": function () {
-      $(this.container.firstChild).trigger("dirty");
-      $(this.container.lastChild).trigger("dirty");
 
-      assert.equals(this.container.childNodes.length, 3);
-    }
-  });
+            $(this.container.firstChild).trigger("dirty");
+            $(this.container.firstChild).trigger("dirty");
+
+            assert.equals(this.container.childNodes.length, 2);
+        },
+
+        "adds new form when all are dirty": function () {
+            $(this.container.firstChild).trigger("dirty");
+            $(this.container.lastChild).trigger("dirty");
+
+            assert.equals(this.container.childNodes.length, 3);
+        }
+    });
 }(sfk));
